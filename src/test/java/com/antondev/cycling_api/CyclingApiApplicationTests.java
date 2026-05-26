3a5e157529b1686053e973e85ccef0ac25b37201
AntonGeTUM <anton.ge@tum.de>
2026-05-26 23:37:52 +0200

    Initial commit

---
+++ src/test/java/com/antondev/cycling_api/CyclingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antondev.cycling_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
